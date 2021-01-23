837d8b3039245959dd221ec6af0da89e1c381d2c
Carlos Manuel Luna Condori <kuko.cmlc@gmail.com>
2021-01-23 01:38:10 -0400

    Add - Unit Test for event emitter on filter field component

--- src/app/components/filter-field/filter-field.component.spec.ts
+++ src/app/components/filter-field/filter-field.component.spec.ts
@@ -1,22 +1,18 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { FormsModule } from "@angular/forms";
 
-import { FilterFieldComponent } from './filter-field.component';
+import { FilterFieldComponent } from "./filter-field.component";
 
-describe('FilterFieldComponent', () => {
+describe("FilterFieldComponent", () => {
   let component: FilterFieldComponent;
   let fixture: ComponentFixture<FilterFieldComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        FormsModule,
-        HttpClientTestingModule
-      ],
-      declarations: [ FilterFieldComponent ]
-    })
-    .compileComponents();
+      imports: [FormsModule, HttpClientTestingModule],
+      declarations: [FilterFieldComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -25,7 +21,15 @@ describe('FilterFieldComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should emit the artist name "Maroon 5" on click', () => {
+    const fixture = TestBed.createComponent(FilterFieldComponent);
+    const component = fixture.componentInstance;
+    component.artistNameInput = "Maroon 5";
+    spyOn(component.searchArtistAlbum, "emit");
+    const nativeElement = fixture.nativeElement;
+    const button = nativeElement.querySelector("button");
+    button.dispatchEvent(new Event("click"));
+    fixture.detectChanges();
+    expect(component.searchArtistAlbum.emit).toHaveBeenCalledWith("Maroon 5");
   });
 });
